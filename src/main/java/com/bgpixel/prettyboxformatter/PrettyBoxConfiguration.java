374ee22d8909d9e4ffc94e3057b12edcad8b37cd
knezmilos13 <knezmilos13@gmail.com>
2018-10-07 21:10:19 +0200

    Boxes are now right-closed; can set a desired width

--- src/main/java/com/bgpixel/prettyboxformatter/PrettyBoxConfiguration.java
+++ src/main/java/com/bgpixel/prettyboxformatter/PrettyBoxConfiguration.java
@@ -5,33 +5,47 @@ import org.jetbrains.annotations.Nullable;
 
 public class PrettyBoxConfiguration {
 
-    private final Boolean prefixEveryPrintWithNewline;
+    @Nullable private final Boolean prefixEveryPrintWithNewline;
+    @Nullable private final Integer charsPerLine;
+    @Nullable private final Boolean wrapContent;
 
-    private PrettyBoxConfiguration(Boolean prefixEveryPrintWithNewline) {
+    private PrettyBoxConfiguration(@Nullable Boolean prefixEveryPrintWithNewline,
+                                   @Nullable Integer charsPerLine,
+                                   @Nullable Boolean wrapContent) {
         this.prefixEveryPrintWithNewline = prefixEveryPrintWithNewline;
+        this.charsPerLine = charsPerLine;
+        this.wrapContent = wrapContent;
     }
 
-    public Boolean isPrefixEveryPrintWithNewline() {
-        return prefixEveryPrintWithNewline;
-    }
+    @Nullable public Boolean getPrefixEveryPrintWithNewline() { return prefixEveryPrintWithNewline; }
+    @Nullable public Integer getCharsPerLine() { return charsPerLine; }
+    @Nullable public Boolean getWrapContent() { return wrapContent; }
 
     public static class Builder {
 
         @Nullable private Boolean prefixEveryPrintWithNewline = false;
+        @Nullable private Integer charsPerLine;
+        @Nullable private Boolean wrapContent;
 
         /** Creates a Builder by copying all values from the PrettyBoxConfiguration instance */
         @NotNull
         public static Builder createFromInstance(@NotNull PrettyBoxConfiguration configuration) {
             Builder builder = new Builder();
-            builder.setPrefixEveryPrintWithNewline(configuration.isPrefixEveryPrintWithNewline());
+            builder.setPrefixEveryPrintWithNewline(configuration.getPrefixEveryPrintWithNewline());
+            builder.setCharsPerLine(configuration.getCharsPerLine());
+            builder.setWrapContent(configuration.getWrapContent());
             return builder;
         }
 
         /** Copies non-null values from the PrettyBoxConfiguration instance */
         @NotNull
         public Builder applyFromInstance(@NotNull PrettyBoxConfiguration configuration) {
-            if(configuration.isPrefixEveryPrintWithNewline() != null)
-                this.prefixEveryPrintWithNewline = configuration.isPrefixEveryPrintWithNewline();
+            if(configuration.getPrefixEveryPrintWithNewline() != null)
+                this.prefixEveryPrintWithNewline = configuration.getPrefixEveryPrintWithNewline();
+            if(configuration.getCharsPerLine() != null)
+                this.charsPerLine = configuration.getCharsPerLine();
+            if(configuration.getWrapContent() != null)
+                this.wrapContent = configuration.getWrapContent();
             return this;
         }
 
@@ -42,9 +56,23 @@ public class PrettyBoxConfiguration {
         }
 
         @NotNull
+        public Builder setCharsPerLine(@Nullable Integer charsPerLine) {
+            this.charsPerLine = charsPerLine;
+            return this;
+        }
+
+        @NotNull
+        public Builder setWrapContent(@Nullable Boolean wrapContent) {
+            this.wrapContent = wrapContent;
+            return this;
+        }
+
+        @NotNull
         public PrettyBoxConfiguration build() {
             return new PrettyBoxConfiguration(
-                    prefixEveryPrintWithNewline);
+                    prefixEveryPrintWithNewline,
+                    charsPerLine,
+                    wrapContent);
         }
 
     }
--- src/main/java/com/bgpixel/prettyboxformatter/PrettyBoxFormatter.java
+++ src/main/java/com/bgpixel/prettyboxformatter/PrettyBoxFormatter.java
@@ -2,28 +2,28 @@ package com.bgpixel.prettyboxformatter;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @SuppressWarnings("WeakerAccess")
 public class PrettyBoxFormatter {
 
     private static final char TOP_LEFT_CORNER = '┌';
+    private static final char MIDDLE_LEFT_CORNER = '├';
     private static final char BOTTOM_LEFT_CORNER = '└';
-    private static final char MIDDLE_CORNER = '├';
+    private static final char TOP_RIGHT_CORNER = '┐';
+    private static final char BOTTOM_RIGHT_CORNER = '┘';
+    private static final char MIDDLE_RIGHT_CORNER = '┤';
     private static final char HORIZONTAL_LINE = '│';
-    private static final String DOUBLE_DIVIDER = "────────────────────────────────────────────────────────";
-    private static final String SINGLE_DIVIDER = "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄";
-    private static final String TOP_BORDER = TOP_LEFT_CORNER + DOUBLE_DIVIDER + DOUBLE_DIVIDER;
-    private static final String BOTTOM_BORDER = BOTTOM_LEFT_CORNER + DOUBLE_DIVIDER + DOUBLE_DIVIDER;
-    private static final String MIDDLE_BORDER = MIDDLE_CORNER + SINGLE_DIVIDER + SINGLE_DIVIDER;
 
     private static final String NEWLINE = System.getProperty("line.separator");
-
 
     @NotNull
     private static final PrettyBoxConfiguration DEFAULT_CONFIGURATION =
             new PrettyBoxConfiguration.Builder()
                     .setPrefixEveryPrintWithNewline(false)
+                    .setCharsPerLine(80)
+                    .setWrapContent(true)
                     .build();
 
     @NotNull
@@ -70,18 +70,84 @@ public class PrettyBoxFormatter {
                                   @NotNull PrettyBoxConfiguration configuration) {
         StringBuilder stringBuilder = new StringBuilder();
 
-        if(configuration.isPrefixEveryPrintWithNewline()) stringBuilder.append(" \n");
+        if(configuration.getPrefixEveryPrintWithNewline()) stringBuilder.append(" \n");
 
-        stringBuilder.append(TOP_BORDER).append(NEWLINE);
+        int horizontalPadding = 1; // TODO
 
+        int contentWidth = configuration.getCharsPerLine() - 2*horizontalPadding - 2;
+        int lineWidth = configuration.getCharsPerLine() - 2;
+
+        List<String> splitLines = new ArrayList<>();
+        int longestContentWidth = 0;
         for(String line : lines) {
-            if(line.length() == 0) stringBuilder.append(MIDDLE_BORDER).append(NEWLINE);
-            else stringBuilder.append(HORIZONTAL_LINE).append(" ").append(line).append(NEWLINE);
+            if(line.length() <= contentWidth) {
+                splitLines.add(line);
+                longestContentWidth = Math.max(line.length(), longestContentWidth);
+            }
+            else {
+                splitLines.addAll(splitLineEveryNChars(line, contentWidth));
+                longestContentWidth = contentWidth;
+            }
         }
 
-        stringBuilder.append(BOTTOM_BORDER);
+
+        if(configuration.getWrapContent()) {
+            contentWidth = longestContentWidth;
+            lineWidth = contentWidth + 2*horizontalPadding;
+        }
+
+
+        stringBuilder.append(TOP_LEFT_CORNER).append(getDoubleDivider(lineWidth))
+                .append(TOP_RIGHT_CORNER).append(NEWLINE);
+
+        for(String line : splitLines) {
+            if(line.length() == 0) {
+                stringBuilder.append(MIDDLE_LEFT_CORNER).append(getSingleDivider(lineWidth))
+                        .append(MIDDLE_RIGHT_CORNER).append(NEWLINE);
+            }
+            else {
+                int rightPadding = horizontalPadding + (contentWidth - line.length());
+                stringBuilder.append(HORIZONTAL_LINE).append(" ").append(line)
+                        .append(getPadding(rightPadding)).append(HORIZONTAL_LINE).append(NEWLINE);
+            }
+        }
+
+        stringBuilder.append(BOTTOM_LEFT_CORNER).append(getDoubleDivider(lineWidth))
+                .append(BOTTOM_RIGHT_CORNER);
 
         return stringBuilder.toString();
+    }
+
+    @NotNull
+    private static List<String> splitLineEveryNChars(@NotNull String string, int partitionSize) {
+        List<String> parts = new ArrayList<>();
+        int len = string.length();
+        for (int i=0; i<len; i+=partitionSize)
+            parts.add(string.substring(i, Math.min(len, i + partitionSize)));
+        return parts;
+    }
+
+    // TODO temp code - implement more advanced handling of drawing elements, possibly in a helper class
+
+    @NotNull
+    private static String getDoubleDivider(int length) {
+        StringBuilder outputBuffer = new StringBuilder(length);
+        for (int i = 0; i < length; i++) outputBuffer.append("─");
+        return outputBuffer.toString();
+    }
+
+    @NotNull
+    private static String getSingleDivider(int length) {
+        StringBuilder outputBuffer = new StringBuilder(length);
+        for (int i = 0; i < length; i++) outputBuffer.append("┄");
+        return outputBuffer.toString();
+    }
+
+    @NotNull
+    private static String getPadding(int length) {
+        StringBuilder outputBuffer = new StringBuilder(length);
+        for (int i = 0; i < length; i++) outputBuffer.append(" ");
+        return outputBuffer.toString();
     }
 
 }
